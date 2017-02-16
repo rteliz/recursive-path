8a6ae5dc12cdf601ecbcf1188983adee83181602
rteliz <9rteliz@stpatrick.org>
2017-02-16 14:02:06 -0600

    day 1

---
+++ path.js
@@ -0,0 +1,15 @@
+//Your code below
+
+
+
+
+//Test code
+let testpath1 = buildPath(0.10,15);
+let testpath2 = buildPath(0.50,15);
+console.log(testpath1);
+console.log(testpath2);
+console.log("---------------------");
+console.log(stroll([true,false,true]],0));
+console.log(stroll([true,true,true]],0));
+console.log(rsj(testpath1,0));
+console.log(rsj(testpath2,0));
